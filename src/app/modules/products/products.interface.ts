68543b4c7f7890ce69b702652d593951de79572e
ashiqee <ashiq.buet73@gmail.com>
2024-07-11 19:00:56 +0600

    [product interface] Done

---
+++ src/app/modules/products/products.interface.ts
@@ -0,0 +1,13 @@
+
+// p_ means Product 
+
+export type TProduct= {
+    p_name: string;
+    p_description:string;
+    p_category:string;
+    p_images:string[];
+    p_price:number;
+    p_stock:number;
+    p_isDeleted: boolean;
+}
+
